21a5048e333a0fd47d0a1a2f2f55339f9e6f74a8
joaquim de campos <joaquimdecampos@smartweb.co.ao>
2022-02-16 23:08:14 +0100

    add

--- perfilGitHub/js/main.js
+++ perfilGitHub/js/main.js
@@ -1,14 +1,17 @@
 //const jason = require("jason")
+var img = document.querySelector("img"),nm=document.querySelector("p"), br=document.getElementById("buscar");
 
-const url ='https://api.github.com/users/kevin9227'
- var img = document.querySelector("img")
+ 
 async function pegar(){
+    const url ='https://api.github.com/users/'+br.value
     const response = await fetch(url)
     .then(response => response.json())
-    .then(json => img.setAttribute("src","src="+json.url).replace)
+    .then(json => {img.setAttribute("src",json.avatar_url)
+                    nm.innerHTML=json.name,
+                    document.querySelector(".descricao").children[0].innerText=json.bio
+    } )
     .catch(Error , console.log("Erro"))
    
 
 }
 
-pegar()

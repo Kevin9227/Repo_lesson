b2af7a61af6bedf47b023369abfe4663b784e926
Joaquim de Campos <43016399+Kevin9227@users.noreply.github.com>
2022-04-20 13:47:43 +0100

    Aleracões

--- perfilGitHub/js/main.js
+++ perfilGitHub/js/main.js
@@ -6,10 +6,17 @@ async function pegar(){
     const url ='https://api.github.com/users/'+br.value
     const response = await fetch(url)
     .then(response => response.json())
-    .then(json => {img.setAttribute("src",json.avatar_url)
+    .then(json => {
+        if(json.name ===undefined){
+            alert("Perfil não encontrado...")
+            return false
+        }else{
+            img.setAttribute("src",json.avatar_url)
                     nm.innerHTML=json.name,
                     document.getElementById("descri").innerText=json.bio
 
+        }
+        
                     //A API TEM LIMITE DE ACESSO DE DADOS
     } )
     .catch(Error , console.log("Erro"))

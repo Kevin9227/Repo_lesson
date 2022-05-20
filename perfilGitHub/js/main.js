85c00ff96f21531911232064df18a2dc4a35869b
Joaquim de Campos <43016399+Kevin9227@users.noreply.github.com>
2022-05-20 17:56:39 +0100

    teste

--- perfilGitHub/js/main.js
+++ perfilGitHub/js/main.js
@@ -1,6 +1,7 @@
 //const jason = require("jason")
 var img = document.querySelector("img"),nm=document.getElementById("nome"), br=document.getElementById("mbusca"),
-local=document.getElementById("local");
+local=document.getElementById("local"),
+site=local=document.getElementById("site");
 
  
 async function pegar(){
@@ -15,6 +16,8 @@ async function pegar(){
             img.setAttribute("src",json.avatar_url)
                     nm.innerHTML=json.name,
                     local.innerHTML=json.location,
+                    site.innerHTML=json.blog,
+                    site.setAttribute("href",json.blog),
                     document.getElementById("descri").innerText=json.bio
 
         }

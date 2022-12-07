c21c2cce568a8cff3c3b00dd570eea5ad09c420a
Joaquim de Campos <43016399+Kevin9227@users.noreply.github.com>
2022-12-07 16:46:04 +0100

    img add

--- pokemon/js/listapok.js
+++ pokemon/js/listapok.js
@@ -7,6 +7,7 @@ test.forEach(nu =>{
 
 'https://pokeapi.co/api/v2/pokemon?limit=9&offset=0';
  let teste =[]
+ let i=0
 async function pegar() {
     const newLocal = "https://pokeapi.co/api/v2/pokemon/"
     const url = newLocal
@@ -14,9 +15,11 @@ async function pegar() {
         .then(response => response.json())
         //.then(json => console.log(json))
         .then(json =>  json.results.forEach(Element =>{
-            document.querySelector(".card").innerHTML +=`
+            console.log(Element)
+            i++
+            document.querySelector(".card_content").innerHTML +=`
             <div class="card"  onclick="">
-            <img src="${Element.url}" alt=""  id="img">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i}.svg" alt=""  id="img">
             <div class="content">
                 <h3>${Element.name}</h3>
             </div>

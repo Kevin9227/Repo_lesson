99ca26fa988b2cd8f8a7d0c3262d8bc5f6109258
Joaquim de Campos <43016399+Kevin9227@users.noreply.github.com>
2023-03-07 15:56:51 +0100

    Comentarios add para legenda

--- pokemon/js/listapok.js
+++ pokemon/js/listapok.js
@@ -1,12 +1,16 @@
 
-let test = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-test.forEach(nu =>{
-    //console.log(nu)
-})
+//SETAR VARIAVES GLOBAIS 
+var lista_item  =[]
+var pag_atual   =1
+var pag_total   =0
+var pag_resumo  =5
+var teste       =[]
+ let i          = 0
+//'https://pokeapi.co/api/v2/pokemon?limit=9&offset=0';
 
-'https://pokeapi.co/api/v2/pokemon?limit=9&offset=0';
- let teste =[], pag_atual=1,pag_total=0,pag_resumo=5
- let i=0
+
+
+// OBTER DADOS da API Modelo 1 para analise.
 async function pegar() {
     const newLocal = "https://pokeapi.co/api/v2/pokemon/"
     const url = newLocal
@@ -27,6 +31,8 @@ async function pegar() {
         </div> ` }))
         .catch(Error, console.log("Falou a requisição "))
 }
+
+//OBETER DADOS da API Modelo 2 para a segunda listagem
 async function fetchData() {
     try {
       const response = await fetch('https://pokeapi.co/api/v2/gender/1');
@@ -57,8 +63,7 @@ async function fetchData() {
 
 
     
-  
-  
+// MODO DE PAGINAÇÃO sobre as listas 
  
 function paginas(){
     const i=0
@@ -82,6 +87,7 @@ function paginas(){
 
 }
 
+// CRIAR BOTÃO PARA RETORNO DA NUMERAÇÃO / LISTA
 function pag_list(){
     const xconteudo = teste
     const pag_uda =Math.ceil(xconteudo.length/ pag_resumo)
@@ -94,7 +100,10 @@ function pag_list(){
         }
        
 
+
 }
+
+// MOSTAR PAGINAÇÃO E CHAMAR A PAGINA ATUALIA OBS: ACHAR UMA MANEIRA DE MUDAR A PAGINA E NÃO INCREMENTAÇÃO.
 function irpag(pag){
     pag_atual =pag
     console.log(pag_atual)
@@ -106,20 +115,3 @@ function irpag(pag){
 //fetchData();
 
 pegar()
-
-// Definindo a data do seu aniversário
- const aniversario = new Date('2023-10-27');
-
-// Obtendo a data atual
-const hoje = new Date();
-
-// Calculando a diferença entre as datas em milissegundos
-const diferenca = aniversario - hoje;
-
-// Convertendo a diferença em dias e horas
-const diasFaltando = Math.floor(diferenca / (1000 * 60 * 60 * 24));
-const horasFaltando = Math.floor((diasFaltando ) * 24);
-const minutos = Math.floor((horasFaltando ) / 60)
-
-// Imprimindo o resultado
-console.log(`Faltam ${diasFaltando} dias e ${horasFaltando} horas e ${minutos} para o seu aniversário!`); 

dff7449c2ef7e16010671d5a3ab97452bab5446b
Joaquim de Campos <43016399+Kevin9227@users.noreply.github.com>
2023-03-10 17:04:58 +0100

    Caminho das img alterado.

--- JC/js/main.js
+++ JC/js/main.js
@@ -1,9 +1,9 @@
 var projecto = [{nome_proje:'Pesquisador de Perfil do GitHub',imagem:'img/perfilGitHub.PNG', descri:'O projeto feito em HTML,CSS e JAVASCRIPT que visa buscar perfis existentes no GitHub.   Projeto feito com objetivo de praticar API do GitHub.',link:'https://kevin9227.github.io/Repo_lesson/perfilGitHub/'}
 
-,{nome_proje:'Bloco de anotações',imagem:'../img/Notas.PNG',descri:'O projeto feito em HTML,CSS e JAVASCRIPT, que visou o estudo da função "localStorage" com fins de praticar. ',link:'https://kevin9227.github.io/Repo_lesson/Notas/'}
+,{nome_proje:'Bloco de anotações',imagem:'img/Notas.PNG',descri:'O projeto feito em HTML,CSS e JAVASCRIPT, que visou o estudo da função "localStorage" com fins de praticar. ',link:'https://kevin9227.github.io/Repo_lesson/Notas/'}
 
-,{nome_proje:'Pokémon Web',imagem:'../img/pokemon.PNG',descri:'O presente projeto visa a criação de um Web Site que na qual lista os Pokémon disponível na API. O site esta a ser desenvolvido com as tec HTML,CSS e JAVASCRIPT. ',link:'https://kevin9227.github.io/Repo_lesson/pokemon/'}
-,{nome_proje:'Desaparecidos S.O.S',imagem:'../img/desaparecidos.PNG',descri:'O presente projeto visa a criação de um Web Site que na qual irá constar todos os desaparecidos relatados a nível nacional, todos os familiares que pretenderem relatar o desaparecimento de um membro da sua família poderá faze-lo na plataforma.  O site esta a ser desenvolvido com as tec HTML,CSS e JAVASCRIPT de inicio.',link:'https://kevin9227.github.io/Repo_lesson/desaparecidos/'}
+,{nome_proje:'Pokémon Web',imagem:'img/pokemon.PNG',descri:'O presente projeto visa a criação de um Web Site que na qual lista os Pokémon disponível na API. O site esta a ser desenvolvido com as tec HTML,CSS e JAVASCRIPT. ',link:'https://kevin9227.github.io/Repo_lesson/pokemon/'}
+,{nome_proje:'Desaparecidos S.O.S',imagem:'img/desaparecidos.PNG',descri:'O presente projeto visa a criação de um Web Site que na qual irá constar todos os desaparecidos relatados a nível nacional, todos os familiares que pretenderem relatar o desaparecimento de um membro da sua família poderá faze-lo na plataforma.  O site esta a ser desenvolvido com as tec HTML,CSS e JAVASCRIPT de inicio.',link:'https://kevin9227.github.io/Repo_lesson/desaparecidos/'}
 ]
 var div = document.createElement('div')
 

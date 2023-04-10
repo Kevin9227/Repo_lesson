5f8c1304bafad65dac504558bb08a2da58a1bc72
joaquim de campos <43016399+Kevin9227@users.noreply.github.com>
2023-04-11 00:00:16 +0100

    Nova alterações

--- PlaySound/js/media.js
+++ PlaySound/js/media.js
@@ -1,23 +1,35 @@
 const inputElement = document.getElementById("myFile");
 inputElement.addEventListener("change", handleFiles, false);
-  const reprodutor =  document.getElementById("my-audio") 
-  const audio = document.createElement("audio");
-function handleFiles() {
-  const fileList = this.files;
- 
-  audio.src = URL.createObjectURL(fileList[0]);
-  reprodutor.src = audio.src
-  window.URL = audio.src
+const reprodutor = document.getElementById("my-audio")
+const audio = document.createElement("audio");
+let iconPlay = document.querySelectorAll("i")
+let titulo = document.getElementById("titulo")
+let lista = document.querySelector(".lista")
 
-  document.body.appendChild(audio);
+
+
+function handleFiles() {
+    const fileList = this.files;
+    titulo.innerHTML = fileList[0].name
+    audio.src = URL.createObjectURL(fileList[0]);
+    reprodutor.src = audio.src
+    document.body.appendChild(audio);
 }
 
 function playAudio() {
-  const audio = document.getElementById("my-audio");
-  audio.play();
+    const audio = document.getElementById("my-audio");
+    audio.play();
+    iconPlay[1].innerHTML = "pause"
 }
 
 function pauseAudio() {
-  const audio = document.getElementById("my-audio");
-  audio.pause();
+    const audio = document.getElementById("my-audio");
+    iconPlay[1].innerHTML = "play_arrow"
+    audio.pause();
+}
+
+
+function myFunction(x) {
+    x.classList.toggle("change");
+    lista.classList.toggle("lista1")
 }

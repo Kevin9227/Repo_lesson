c89f16e488ef447f5f0816a963987d9182cd0c36
joaquim de campos <43016399+Kevin9227@users.noreply.github.com>
2023-04-12 00:00:59 +0100

    Nova alterações

--- PlaySound/js/media.js
+++ PlaySound/js/media.js
@@ -1,10 +1,12 @@
-const inputElement = document.getElementById("myFile");
-inputElement.addEventListener("change", handleFiles, false);
+const ficheiros = document.getElementById("myFile");
+ficheiros.addEventListener("change", handleFiles, false);
 const reprodutor = document.getElementById("my-audio")
 const audio = document.createElement("audio");
 let iconPlay = document.querySelectorAll("i")
 let titulo = document.getElementById("titulo")
 let lista = document.querySelector(".lista")
+let gif = document.querySelector("img")
+
 
 
 
@@ -14,22 +16,36 @@ function handleFiles() {
     audio.src = URL.createObjectURL(fileList[0]);
     reprodutor.src = audio.src
     document.body.appendChild(audio);
+    listdereproducao();
 }
 
 function playAudio() {
     const audio = document.getElementById("my-audio");
     audio.play();
-    iconPlay[1].innerHTML = "pause"
+    iconPlay[1].innerHTML = "pause";
+    gif.style.opacity = "100%"
 }
 
 function pauseAudio() {
     const audio = document.getElementById("my-audio");
     iconPlay[1].innerHTML = "play_arrow"
     audio.pause();
+    gif.style.opacity = "0%"
 }
 
 
 function myFunction(x) {
     x.classList.toggle("change");
     lista.classList.toggle("lista1")
+}
+
+function listdereproducao() {
+    if (ficheiros.files.length > 0) {
+        for (i = 0; i <= ficheiros.files.length; i++) {
+            lista.innerHTML += `
+                <p>${ficheiros.files[i].name}</p>
+                `
+
+        }
+    }
 }

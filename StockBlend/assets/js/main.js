85627595b5d9500bab3181b4a4366e1e14beea3f
Joaquim de Campos <43016399+Kevin9227@users.noreply.github.com>
2023-12-14 14:05:46 +0100

    novas alterações...

--- StockBlend/assets/js/main.js
+++ StockBlend/assets/js/main.js
@@ -1,2 +1,23 @@
-let header =document.getElementById("red");
-if do
+
+
+document.onload(openCity(event,'London'))
+
+function activo(evt,cityName){
+ 
+  document.getElementById(cityName).style.display="block";
+  evt.currentTarget.className += " active";
+}
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}

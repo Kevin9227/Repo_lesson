7c9b080b94f2f0906a3485acabf8c1724eb234e8
Joaquim de Campos <43016399+Kevin9227@users.noreply.github.com>
2024-05-06 18:00:19 +0100

    Alterações..

--- StockBlend/assets/js/main.js
+++ StockBlend/assets/js/main.js
@@ -1,14 +1,14 @@
 
+ 
+/*document.onload(openCity(event,'Javascript'))
 
-document.onload(openCity(event,'London'))
-
-function activo(evt,cityName){
+function activo(event,cityName){
  
   document.getElementById(cityName).style.display="block";
   evt.currentTarget.className += " active";
 }
 
-function openCity(evt, cityName) {
+function openCity(event, cityName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -20,4 +20,15 @@ function openCity(evt, cityName) {
   }
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
+    
 }
+ */
+window.addEventListener('scroll', function() {
+    // Verificar se a posição da rolagem é maior que 100 pixels
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 100) {
+        // Faça algo quando a rolagem atingir mais de 100 pixels
+     document.getElementById('red').style.position ="fixed";
+    }else{
+           document.getElementById('red').style.position ="relative";
+    }
+});
